ce5a9866df369de3aa8142756c4692eba0bcf501
Natalia <nataliarios325@gmail.com>
2020-11-04 10:41:39 -0500

    Add macro to index.html

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+
 from wtform_fields import *
 
 app = Flask(__name__)
--- wtform_fields.py
+++ wtform_fields.py
@@ -3,15 +3,15 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
-    """ Forma de registro"""
 
     username = StringField('username_label',
-         validators=[InputRequired(message="Username required"),
-         Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
-    password = PasswordField ('password_label',
-         validators=[InputRequired(message="Password required"),
-         Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+        validators=[InputRequired(message="Username required"),
+        Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+    password = PasswordField('password_label',
+        validators=[InputRequired(message="Password required"),
+        Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
     confirm_pswd = PasswordField('confirm_pswd_label',
-         validators=[InputRequired(message="Password required"),
-         EqualTo('password', message="Passwords must match")])
+        validators=[InputRequired(message="Password required"),
+        EqualTo('password', message="Passwords must match")])
+
     submit_button = SubmitField('Create')
